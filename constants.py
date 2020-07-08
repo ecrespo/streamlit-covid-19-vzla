01ffe34579c0c807335c0ef42638972d21c679c8
Ernesto Crespo <ecrespo@gmail.com>
2020-07-07 20:04:25 -0400

    Added constants

---
+++ constants.py
@@ -0,0 +1,9 @@
+
+
+var = {
+    "path": "/home/ernesto/proyectos/streamlit-covid-19-vzla/covid19/csse_covid_19_data/csse_covid_19_daily_reports/",
+    "directory" : "./data/",
+    "start": "01-01-2020",
+    "url1": "https://docs.google.com/spreadsheets/d/e/2PACX-1vQI4s0no2TS1dYxbv82nhKD7iz8fbDGwdsOI4kzJ0cg3gjOR51KIw_rNOff97Xic_fRQD41xmsDGUfM/pub?gid=1029482781&single=true&output=csv",
+    "url2": "https://docs.google.com/spreadsheets/d/e/2PACX-1vTT8ef-uVa5q_5kBYbVXeEpRCW8gOJHlWhHGrH8dQ704D64_yNaMMjvkzdgD9YweSBQ-GyqnGLLasvK/pub?gid=1574343536&single=true&output=csv"
+}
